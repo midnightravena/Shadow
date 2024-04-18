43480fc93cc248a8544579da7bd90de30471995a
Xenin <126798153+xeninftw@users.noreply.github.com>
2024-04-18 19:16:55 +0530

    Update rules.dart

--- package/compiler/lib/scanner/rules/rules.dart
+++ package/compiler/lib/scanner/rules/rules.dart
@@ -5,3 +5,20 @@ import '../utils.dart';
 import 'identifier.dart';
 import 'number.dart';
 import 'string.dart';
+
+typedef ScannerRuleMatchFn = bool Function(
+  Scanner scanner,
+  InputIteration current,
+);
+
+typedef ScannerRuleReadFn = Token Function(
+  Scanner scanner,
+  InputIteration current,
+);
+
+class ScannerCustomRule {
+  const ScannerCustomRule(this.matches, this.scan);
+
+  final ScannerRuleMatchFn matches;
+  final ScannerRuleReadFn scan;
+}
