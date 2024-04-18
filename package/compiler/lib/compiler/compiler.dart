46e140f9993b5e0367850c8cc1b9f8ea27d042e1
Zerex <110465211+zerexly@users.noreply.github.com>
2024-04-18 11:22:41 +0530

    Update compiler.dart

--- package/compiler/lib/compiler/compiler.dart
+++ package/compiler/lib/compiler/compiler.dart
@@ -7,3 +7,29 @@ import '../scanner/exports.dart';
 import '../token/exports.dart';
 import 'parser/exports.dart';
 import 'state.dart';
+
+enum CompilerMode {
+  function,
+  script,
+}
+
+class CompilerOptions {
+  CompilerOptions({
+    this.disablePrint = false,
+  });
+
+  final bool disablePrint;
+}
+
+class Compiler {
+  Compiler._(
+    this.scanner, {
+    required this.mode,
+    required this.root,
+    required this.modulePath,
+    required this.moduleIndex,
+    required this.modules,
+    required this.constants,
+    required this.options,
+    this.parent,
+  });
