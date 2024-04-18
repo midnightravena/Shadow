ce4ef33d69bb199c6fe94bc112bf09e37b898179
Sahil <151955025+sahilarun@users.noreply.github.com>
2024-04-18 10:39:40 +0530

    Update rules.dart
    - Incomplete ParseruleFn

--- package/compiler/lib/compiler/parser/rules.dart
+++ package/compiler/lib/compiler/parser/rules.dart
@@ -2,3 +2,18 @@ import '../../token/exports.dart';
 import '../compiler.dart';
 import 'parser.dart';
 import 'precedence.dart';
+
+typedef ParseruleFn = void Function(Compiler compiler);
+
+class Parserule {
+  const Parserule({
+    this.precedence = Precedence.none,
+    this.prefix,
+    this.infix,
+  });
+
+  final Precedence precedence;
+  final ParseruleFn? prefix;
+  final ParseruleFn? infix;
+
+  static const Parserule none = Parserule();
