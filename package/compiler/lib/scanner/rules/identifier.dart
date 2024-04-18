2575cefa046294a9ea33dc09a75e30ec7e35e72b
Sahil <151955025+sahilarun@users.noreply.github.com>
2024-04-18 19:15:19 +0530

    Fixed identifier.dart [3/5]

--- package/compiler/lib/scanner/rules/identifier.dart
+++ package/compiler/lib/scanner/rules/identifier.dart
@@ -31,3 +31,34 @@ abstract class IdentifierScanner {
     Tokens.awaitKw,
     Tokens.onlyKw,
   };
+
+  static final Map<String, Tokens> keywordsMap = keywords.asNameMap().map(
+        (final _, final Tokens x) => MapEntry<String, Tokens>(x.code, x),
+      );
+
+  static bool matches(
+    final Scanner scanner,
+    final InputIteration current,
+  ) =>
+      LexerUtils.isAlphaNumeric(current.char);
+
+  static Token readIdentifier(
+    final Scanner scanner,
+    final InputIteration start,
+  ) {
+    final ShadowStringBuffer buffer = ShadowStringBuffer(start.char);
+    InputIteration current = start;
+    while (!scanner.input.isEndOfLine()) {
+      current = scanner.input.peek();
+      if (!LexerUtils.isAlphaNumeric(current.char)) break;
+      buffer.write(current.char);
+      scanner.input.advance();
+    }
+    final String output = buffer.toString();
+    return Token(
+      keywordsMap[output] ?? Tokens.identifier,
+      output,
+      Span(start.point, current.point),
+    );
+  }
+}
