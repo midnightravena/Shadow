a86f6997d986dfcb79ddbae059195f7108aaf41e
Xenin <126798153+xeninftw@users.noreply.github.com>
2024-04-18 14:05:26 +0530

    Update disassembler.dart

--- package/compiler/lib/disassembler/disassembler.dart
+++ package/compiler/lib/disassembler/disassembler.dart
@@ -1,2 +1,23 @@
 import 'package:shared/shared.dart';
 import 'output.dart';
+
+class Disassembler {
+  Disassembler(this.program, this.chunk, this.output);
+
+  final ProgramConstant program;
+  final Chunk chunk;
+  final DisassemblerOutput output;
+
+  int ip = 0;
+
+  void dissassemble({
+    final bool printHeader = true,
+  }) {
+    if (printHeader) {
+      write('Offset{s}OpCode{s}Position');
+    }
+    while (ip < chunk.length) {
+      ip += dissassembleInstruction();
+      ip++;
+    }
+  }
