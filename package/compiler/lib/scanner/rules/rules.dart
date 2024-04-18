8e94613e75bc8a45ac354108720ebaf92cce7913
Zerex <110465211+zerexly@users.noreply.github.com>
2024-04-18 19:16:25 +0530

    Create rules.dart

---
+++ package/compiler/lib/scanner/rules/rules.dart
@@ -0,0 +1,7 @@
+import '../../lexer/exports.dart';
+import '../../token/exports.dart';
+import '../scanner.dart';
+import '../utils.dart';
+import 'identifier.dart';
+import 'number.dart';
+import 'string.dart';
