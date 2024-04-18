2f082fb6667d9f9c91ae12132050af78eeea38a4
Sahil <151955025+sahilarun@users.noreply.github.com>
2024-04-18 09:30:42 +0530

    Update parser.dart

--- package/compiler/lib/compiler/parser/parser.dart
+++ package/compiler/lib/compiler/parser/parser.dart
@@ -4,3 +4,47 @@ import '../../token/exports.dart';
 import '../compiler.dart';
 import 'precedence.dart';
 import 'rules.dart';
+
+abstract class Parser {
+  static Future<void> parseStatement(final Compiler compiler) async {
+    if (compiler.match(Tokens.braceLeft)) {
+      return parseBlockStatement(compiler);
+    }
+    if (compiler.match(Tokens.ifKw)) {
+      return parseIfStatement(compiler);
+    }
+    if (compiler.match(Tokens.whileKw)) {
+      return parseWhileStatement(compiler);
+    }
+    if (compiler.match(Tokens.breakKw)) {
+      return parseBreakStatement(compiler);
+    }
+    if (compiler.match(Tokens.continueKw)) {
+      return parseContinueStatement(compiler);
+    }
+    if (compiler.match(Tokens.returnKw)) {
+      return parseReturnStatement(compiler);
+    }
+    if (compiler.match(Tokens.throwKw)) {
+      return parseThrowStatement(compiler);
+    }
+    if (compiler.match(Tokens.tryKw)) {
+      return parseTryCatchStatement(compiler);
+    }
+    if (compiler.match(Tokens.importKw)) {
+      return parseImportStatement(compiler);
+    }
+    if (compiler.match(Tokens.whenKw)) {
+      return parseWhenStatement(compiler);
+    }
+    if (compiler.match(Tokens.matchKw)) {
+      return parseMatchStatement(compiler);
+    }
+    if (compiler.match(Tokens.printKw)) {
+      return parsePrintStatement(compiler);
+    }
+    if (compiler.match(Tokens.forKw)) {
+      return parseForStatement(compiler);
+    }
+    return parseExpressionStatement(compiler);
+  }
