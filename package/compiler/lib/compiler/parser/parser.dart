675097e7d3bcc2764d5a3ed944e36413c6cb4af9
Zerex <110465211+zerexly@users.noreply.github.com>
2024-04-18 10:10:11 +0530

    Update parser.dart

--- package/compiler/lib/compiler/parser/parser.dart
+++ package/compiler/lib/compiler/parser/parser.dart
@@ -720,3 +720,17 @@ abstract class Parser {
     parseExpression(compiler);
     compiler.patchJump(elseJump);
   }
+
+  static void parseNullOr(
+    final Compiler compiler, {
+    final Precedence precedence = Precedence.or,
+    final void Function()? beforePatch,
+  }) {
+    final int elseJump = compiler.emitJump(OpCodes.opJumpIfNull);
+    final int endJump = compiler.emitJump(OpCodes.opJump);
+    compiler.patchJump(elseJump);
+    compiler.emitOpCode(OpCodes.opPop);
+    parsePrecedence(compiler, precedence);
+    beforePatch?.call();
+    compiler.patchJump(endJump);
+  }
