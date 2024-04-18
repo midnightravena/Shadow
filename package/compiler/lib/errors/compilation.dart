3325663a012717a5958e2f58877fb35a6dd54a60
Sahil <151955025+sahilarun@users.noreply.github.com>
2024-04-18 14:12:57 +0530

    Update compilation.dart

--- package/compiler/lib/errors/compilation.dart
+++ package/compiler/lib/errors/compilation.dart
@@ -1,1 +1,17 @@
 import '../token/exports.dart';
+
+class CompilationException implements Exception {
+  CompilationException(this.module, this.text);
+
+  factory CompilationException.illegalToken(
+    final String module,
+    final Token token,
+  ) =>
+      CompilationException(
+        module,
+        <String>[
+          'Illegal token "${token.type.code}" found at ${token.span}',
+          if (token.error != null)
+            '(${token.error}${token.errorSpan != null ? ' at ${token.errorSpan}' : ''})',
+        ].join(' '),
+      );
