203f08fe61ea57975c7585350f4c086b1c1c4200
Xenin <126798153+xeninftw@users.noreply.github.com>
2024-04-19 12:22:36 +0530

    Create interpret.dart

---
+++ package/cli/lib/commands/interpret.dart
@@ -0,0 +1,73 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:args/command_runner.dart';
+import 'package:vm/vm.dart';
+import 'package:collection/collection.dart';
+import 'package:path/path.dart' as p;
+import '../utils.dart';
+
+class InterpretCommand extends Command<Future<void>> {
+  InterpretCommand() {
+    argParser.addFlag(
+      disablePrintFlag,
+      help: 'Disable native print function.',
+      negatable: false,
+    );
+  }
+
+  @override
+  final String name = 'interpret';
+
+  @override
+  final List<String> aliases = <String>['run-compiled', 'execute-compiled'];
+
+  @override
+  final String description = 'Interpret a compiled program.';
+
+  @override
+  String get invocation => '${runner!.executableName} $name <path/to/compiled>';
+
+  @override
+  Future<void> run() async {
+    final String? compiledFilePathRaw = argResults!.rest.firstOrNull;
+    final bool disablePrint = argResults![disablePrintFlag] as bool;
+    if (compiledFilePathRaw == null) {
+      printInvalidInvocation('Specify a file to interpret.');
+      return;
+    }
+
+    final String compiledFilePath = p.absolute(compiledFilePathRaw);
+    final File compiledFile = File(compiledFilePath);
+    if (!(await compiledFile.exists())) {
+      print('Specified file "${compiledFile.path}" does not exist.');
+      return;
+    }
+
+    late final ProgramConstant program;
+    try {
+      final String content = await compiledFile.readAsString();
+      final List<dynamic> parsed = json.decode(content) as List<dynamic>;
+      program = ProgramConstant.deserialize(parsed);
+    } catch (err) {
+      print('Parsing "${compiledFile.path}" failed.');
+      println();
+      print(err);
+      return;
+    }
+
+    try {
+      final VM vm = VM(
+        program,
+        VMOptions(
+          disablePrint: disablePrint,
+          printPrefix: '',
+        ),
+      );
+      await vm.run();
+    } catch (err) {
+      print(err);
+    }
+  }
+
+  static const String disablePrintFlag = 'disable-print';
+}
